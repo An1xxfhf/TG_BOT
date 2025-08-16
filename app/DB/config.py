dd2e29e882d21fff11027adba76545690686f0b6
Yaroslav <korolev.yaroslav07@gmail.com>
2025-08-16 22:58:03 +0300

    First commit

--- app/DB/config.py
+++ app/DB/config.py
@@ -3,15 +3,24 @@ from dotenv import load_dotenv
 load_dotenv()
 class Settings(BaseSettings):
     DB_HOST: str
-    DB_PORT: str
+    DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
-    model_config = SettingsConfigDict(env_file="../../.env")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
     @property
-    def DB_URL_ASP(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+    def DB_URL_ASP(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.DB_USER}:{self.DB_PASS}"
+            f"@{self.DB_HOST}:{self.DB_PORT}"
+            f"/{self.DB_NAME}"
+        )
 
 settings = Settings()
